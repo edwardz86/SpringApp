e104a8df1efdfea077aec6f73941d3f9f52ae43f
edwardz86 <tomaszstolarczyk86@gmail.com>
2016-10-05 12:04:44 +0200

    Junit initialization

--- src/main/java/spring/app/mvc/HomeController.java
+++ src/main/java/spring/app/mvc/HomeController.java
@@ -24,6 +24,8 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		// http://localhost:8084/mvc/
+		
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
---
+++ src/test/java/spring/app/mvc/TestJUnitSpringExample.java
@@ -0,0 +1,13 @@
+package spring.app.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJUnitSpringExample {
+
+	@Test
+    public void testOrderService() {
+		assertTrue("test".contains("test"));
+    }
+}
