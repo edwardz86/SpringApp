80a328fdc7afdf1ce4b2d6a6548c99f66c340dbb
edwardz86 <tomaszstolarczyk86@gmail.com>
2016-10-07 12:49:49 +0200

    JPA

--- src/main/java/spring/app/controller/HomeController.java
+++ src/main/java/spring/app/controller/HomeController.java
@@ -1,15 +1,26 @@
-package spring.app.mvc;
+package spring.app.controller;
 
 import java.text.DateFormat;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import spring.app.model.Language;
 
 /**
  * Handles requests for the application home page.
@@ -19,12 +30,30 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+
+	private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this. entityManager = entityManager;
+    }
+	
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		// http://localhost:8084/mvc/
+		
+		int rowCount = this.jdbcTemplate.queryForInt("select count(0) from languages");
+		System.out.println(rowCount);
+		
+		List<Language> ListOfEmailDomains = entityManager.createQuery("SELECT e FROM Language e").getResultList();
+		System.out.println(ListOfEmailDomains.size());
 		
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
---
+++ src/main/java/spring/app/model/Language.java
@@ -0,0 +1,39 @@
+package spring.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "languages")
+public class Language {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_language")
+	private Integer idLanguage;
+
+	private String name;
+
+	public Integer getIdLanguage() {
+		return idLanguage;
+	}
+
+	public void setIdLanguage(Integer idLanguage) {
+		this.idLanguage = idLanguage;
+	}
+
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
